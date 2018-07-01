84d237b298310dff093dd3ad69de23b8115c7f2c
DanielMller <40738675+DanielMller@users.noreply.github.com>
2018-07-01 17:07:13 +0200

    Update PidTuner.py

--- PidTuner.py
+++ PidTuner.py
@@ -1,11 +1,14 @@
 import numpy as np 
-import casadi as ca
+#import casadi as ca
+import julia
 
 class PidTuner:
 
 	def __init__(self, modelFitter, dt):
 		# get order, then call .predict() with 2 args
 		self.modelFitter = modelFitter
+		j=julia.Julia()
+		pid=j.include("PID_Optimizer")
 
 	# Objective fct: Depends on unknown y-states, known reference
 	def getObjective(self, states, ref):
@@ -17,6 +20,22 @@ class PidTuner:
 		print("empty")
 
 	def fitPid(self):
-		print("lol")
+		### Inputs
+		#Kontinuierliche Matrizen A,B,C,D 
+		#maximales überschwingen
+		#endzeit tf
+		#anzahl der disk punkte
+		#unterer bound für u
+		#oberer bound für u
+		### Outputs
+		# x[0] -status ob program optimal gelöst wurde
+		# x[1] [P,I,D] Parameter
+		# x[2] Output y
+		# x[3] Timevector t
+		x=self.pid(A,B,C,D,SollWert,max_overshoot,tf,Number_Of_Disc_Points,u_lower_bound,u_upper_bound)
+		PID=x[1]
+		print("P=" PID[0])
+		print("I=" PID[1])
+		print("D=" PID[2])
 
 
